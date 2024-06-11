aa6634a6a706fd3eed5b529c0f0f390b7d6efe41
Oooooooku <tanutanu1103@icloud.com>
2024-06-11 14:45:37 +0900

    ＋ーボタン追加＆CSS調整

--- js/lunch.js
+++ js/lunch.js
@@ -1,9 +1,24 @@
-function money_total(){
-    money = 0;
-    for (i=0; i<document.menuForm.length; i++){
-      if (document.menuForm.elements[i].checked){
-        money += eval(document.menuForm.elements[i].value);
-      }
-    }
-    document.totalForm.total.value = money + " 円";
+function money_total() {
+  let total = 0;
+  const quantities = document.querySelectorAll('input[type="number"]');
+  quantities.forEach(quantity => {
+      const price = parseInt(quantity.dataset.price, 10);
+      const count = parseInt(quantity.value, 10);
+      total += price * count;
+  });
+  document.totalForm.total.value = `¥${total}`;
+}
+
+function increaseValue(id) {
+  const quantityInput = document.getElementById(id);
+  quantityInput.value = parseInt(quantityInput.value, 10) + 1;
+  money_total();
+}
+
+function decreaseValue(id) {
+  const quantityInput = document.getElementById(id);
+  if (quantityInput.value > 0) {
+      quantityInput.value = parseInt(quantityInput.value, 10) - 1;
+      money_total();
   }
+}
